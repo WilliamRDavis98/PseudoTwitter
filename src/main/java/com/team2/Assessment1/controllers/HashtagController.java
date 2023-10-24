3cf3dabb79a1066f1de7a8c3440596d5689a3591
Brennan <n3rdtp@gmail.com>
2023-10-24 15:36:50 -0500

    Implemented GET /tags endpoint

--- src/main/java/com/team2/Assessment1/controllers/HashtagController.java
+++ src/main/java/com/team2/Assessment1/controllers/HashtagController.java
@@ -1,7 +1,13 @@
 package com.team2.Assessment1.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.team2.Assessment1.dtos.HashtagDto;
+import com.team2.Assessment1.services.HashtagService;
 
 //Import Services
 
@@ -11,5 +17,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/tags")
 public class HashtagController {
-
+	private final HashtagService hashtagService;
+	
+	@GetMapping
+	public List<HashtagDto> getAllHashtags() {
+		return hashtagService.getAllHashtags();
+	}
 }
--- src/main/java/com/team2/Assessment1/mappers/HashtagMapper.java
+++ src/main/java/com/team2/Assessment1/mappers/HashtagMapper.java
@@ -1,5 +1,7 @@
 package com.team2.Assessment1.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
 import com.team2.Assessment1.dtos.HashtagDto;
@@ -10,5 +12,6 @@ public interface HashtagMapper {
 	
 	HashtagDto entityToDto(Hashtag entity);
 	Hashtag dtoToEntity(HashtagDto dto);
+	List<HashtagDto> entitiesToDtos(List<Hashtag> findAll);
 
 }
